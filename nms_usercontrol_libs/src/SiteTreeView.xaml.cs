eabd37752781288a6fd3d2ac08a52cbb525f3473
Albert.Shi <sdqdshixin@126.com>
2013-03-13 14:26:28 +0800

    Add new png.
    Add some new .png pictures.

--- nms_usercontrol_libs/src/SiteTreeView.xaml.cs
+++ nms_usercontrol_libs/src/SiteTreeView.xaml.cs
@@ -21,7 +21,11 @@ namespace nms_usercontrol_libs.src
     public partial class SiteTreeView : UserControl
     {
         private List<SiteModel> SiteLists = new List<SiteModel>();
-        private const string RPTICON = "/nms_usercontrol_libs;component/images/computer_24.png";
+        private const string RPTICON = "/nms_usercontrol_libs;component/images/wireless_16.png";
+        private const string CITYICON = "/nms_usercontrol_libs;component/images/city_16.png";
+        private const string EDITICON = "/nms_usercontrol_libs;component/images/edit_16.png";
+        //private const string ADDICON = "/nms_usercontrol_libs;component/images/add_16.png";
+
 
         public SiteTreeView()
         {
@@ -38,57 +42,118 @@ namespace nms_usercontrol_libs.src
         private void ShowTreeView()
         {
             List<PropertyNodeItem> itemList = new List<PropertyNodeItem>();
+
             PropertyNodeItem node1 = new PropertyNodeItem()
             {
-                DisplayName = "Node No.1",
+                DisplayName = "Guangzhou",
                 Name = "This is the discription of Node1. This is a folder.",
-                Icon = RPTICON,
+                Icon = CITYICON,
             };
 
             PropertyNodeItem node1tag1 = new PropertyNodeItem()
             {
-                DisplayName = "Tag No.1",
+                DisplayName = "RPT00012210",
                 Name = "This is the discription of Tag 1. This is a tag.",
                 Icon = RPTICON,
-                EditIcon = "/nms_usercontrol_libs;component/images/refresh_24.png"
+                EditIcon = EDITICON,
             };
             node1.Children.Add(node1tag1);
 
             PropertyNodeItem node1tag2 = new PropertyNodeItem()
             {
-                DisplayName = "Tag No.2",
+                DisplayName = "RPT00011882",
                 Name = "This is the discription of Tag 2. This is a tag.",
-                Icon = "/nms_usercontrol_libs;component/images/computer_24.png",
-                EditIcon = "/nms_usercontrol_libs;component/images/refresh_24.png"
+                Icon = RPTICON,
+                EditIcon = EDITICON,
             };
             node1.Children.Add(node1tag2);
             itemList.Add(node1);
 
             PropertyNodeItem node2 = new PropertyNodeItem()
             {
-                DisplayName = "Node No.2",
+                DisplayName = "Beijing",
                 Name = "This is the discription of Node 2. This is a folder.",
-                Icon = RPTICON,
+                Icon = CITYICON,
             };
 
             PropertyNodeItem node2tag3 = new PropertyNodeItem()
             {
-                DisplayName = "Tag No.3",
+                DisplayName = "FO121093488",
                 Name = "This is the discription of Tag 3. This is a tag.",
                 Icon = RPTICON,
-                EditIcon = "/nms_usercontrol_libs;component/images/refresh_24.png"
+                EditIcon = EDITICON
             };
             node2.Children.Add(node2tag3);
 
             PropertyNodeItem node2tag4 = new PropertyNodeItem()
             {
-                DisplayName = "Tag No.4",
+                DisplayName = "FO121096097",
                 Name = "This is the discription of Tag 4. This is a tag.",
                 Icon = RPTICON,
-                EditIcon = "/nms_usercontrol_libs;component/images/refresh_24.png"
+                EditIcon = EDITICON
             };
             node2.Children.Add(node2tag4);
             itemList.Add(node2);
+
+
+            PropertyNodeItem node3 = new PropertyNodeItem()
+            {
+                DisplayName = "Shanghai",
+                Name = "This is the discription of Node 2. This is a folder.",
+                Icon = CITYICON,
+            };
+
+            PropertyNodeItem node3sub1 = new PropertyNodeItem()
+            {
+                DisplayName = "Qingpu",
+                Name = "This is the discription of Node 2. This is a folder.",
+                Icon = CITYICON,
+            };
+
+            PropertyNodeItem node3sub1sub1 = new PropertyNodeItem()
+            {
+                DisplayName = "RPT01220123",
+                Name = "This is the discription of Node 2. This is a folder.",
+                Icon = RPTICON,
+                EditIcon = EDITICON
+            };
+            node3sub1.Children.Add(node3sub1sub1);
+
+            PropertyNodeItem node3sub1sub2 = new PropertyNodeItem()
+            {
+                DisplayName = "RPT8872137",
+                Name = "This is the discription of Node 2. This is a folder.",
+                Icon = RPTICON,
+                EditIcon = EDITICON
+            };
+            node3sub1.Children.Add(node3sub1sub2);
+            node3.Children.Add(node3sub1);
+
+            PropertyNodeItem node3sub2 = new PropertyNodeItem()
+            {
+                DisplayName = "Minhang",
+                Name = "This is the discription of Node 2. This is a folder.",
+                Icon = CITYICON,
+            };
+
+            PropertyNodeItem node3sub2sub1 = new PropertyNodeItem()
+            {
+                DisplayName = "RPT001232112",
+                Name = "This is the discription of Node 2. This is a folder.",
+                Icon = RPTICON,
+                EditIcon = EDITICON
+            };
+            node3sub2.Children.Add(node3sub2sub1);
+            node3.Children.Add(node3sub2);
+
+            itemList.Add(node3);
+            itemList.Add(node3);
+            itemList.Add(node3);
+            itemList.Add(node1);
+            itemList.Add(node1);
+            itemList.Add(node3);
+            itemList.Add(node2);
+            itemList.Add(node3);
 
             this.SiteListTreeView.ItemsSource = itemList;
         }
@@ -96,59 +161,25 @@ namespace nms_usercontrol_libs.src
         private void BtnRepeaterEdit_Click(object sender, MouseButtonEventArgs e)
         {
 
-            MessageBox.Show("DDDDDDDDDDDD");
+            MessageBox.Show("Edit button click event test");
         }
 
-
-        /*
-        public void BindTreeView()
+        private void BtnRepeaterAdd_Click(object sender, MouseButtonEventArgs e)
         {
-            PropertyNodeItem node1 = new PropertyNodeItem("a1", "中国", 0);
-            PropertyNodeItem node2 = new PropertyNodeItem("a2", "北京市", 1);
-            PropertyNodeItem node3 = new PropertyNodeItem("a3", "吉林省", 1);
-            PropertyNodeItem node4 = new PropertyNodeItem("a4", "上海市", 1);
-            node1.Items.Add(node2);
-            node1.Items.Add(node3);
-            node1.Items.Add(node4);
-            PropertyNodeItem node5 = new PropertyNodeItem("5", "海淀区", 2);
-            PropertyNodeItem node6 = new PropertyNodeItem("6", "朝阳区", 2);
-            PropertyNodeItem node7 = new PropertyNodeItem("7", "大兴区", 2);
-            node2.Items.Add(node5);
-            node2.Items.Add(node6);
-            node2.Items.Add(node7);
-            PropertyNodeItem node8 = new PropertyNodeItem("8", "白山市", 2);
-            PropertyNodeItem node9 = new PropertyNodeItem("9", "长春市", 2);
-            node3.Items.Add(node8);
-            node3.Items.Add(node9);
-            PropertyNodeItem node10 = new PropertyNodeItem("10", "抚松县", 3);
-            PropertyNodeItem node11 = new PropertyNodeItem("11", "靖宇县", 3);
-            node8.Items.Add(node10);
-            node8.Items.Add(node11);
-            this.SiteListTreeView.Items.Clear();
-            this.SiteListTreeView.Items.Add(node1);
-        }*/
+            MessageBox.Show("Add button click event test");
+        }
 
+        private void BtnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("BtnAddNew_Click event test");
+        }
     }
-
-    /*
-     public class PropertyNodeItem : TreeViewItem
-     {
-         public int Level { get; set; }
-         public string Name { get; set; }
-
-         public PropertyNodeItem(string name, string DisplayName, int level)
-         {
-             this.Level = level;
-             this.Header = DisplayName;
-             this.Name = name;
-             // 在此点下面插入创建对象所需的代码。
-         }
-     }*/
 
     internal class PropertyNodeItem
     {
         public string Icon { get; set; }
         public string EditIcon { get; set; }
+        public string AddIcon { get; set; }
         public string DisplayName { get; set; }
         public string Name { get; set; }
 
